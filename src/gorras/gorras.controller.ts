19e9d4b9d3c58458fd84525bfaf81fff067bf40c
Cristopher Gonzáles Pérez <cristobussines@gmail.com>
2025-08-21 02:16:23 -0600

    Se cambian parametros de validacion de gorras

--- src/gorras/gorras.controller.ts
+++ src/gorras/gorras.controller.ts
@@ -2,25 +2,14 @@ import { Controller, Get, Render, Param } from '@nestjs/common';
 import { GorrasService } from './gorras.service';
 import { DropConfigService } from '../drops/drop-config.service';
 
-@Controller('drops/:drop/validar')
+@Controller('drops/:drop')
 export class GorrasController {
   constructor(
     private readonly gorrasService: GorrasService,
     private readonly dropConfig: DropConfigService,
   ) {}
 
-  @Get(':id')
-  @Render('validacion')
-  async validarGorra(
-    @Param('drop') drop: string,
-    @Param('id') id: string,
-  ) {
-    const gorra = await this.gorrasService.getGorraById(id, drop);
-    const dropInfo = this.dropConfig.getDrop(drop);
-    return { gorra, drop: dropInfo };
-  }
-
-  @Get('alerta/:id')
+  @Get('idgorra/:id')
   @Render('validacion-alerta')
   async validarConAlerta(
     @Param('drop') drop: string,
